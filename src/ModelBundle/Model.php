568cc0347fa87ac3710c80ba248ea9d670388158
Quentin Bisson <bissoqu1@Quentin-PC.(none)>
2013-01-12 00:18:09 +0100

    Beginning Doctrine

---
+++ src/ModelBundle/Model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Description of Model
+ *
+ * @author bissoqu1
+ */
+
+namespace ModelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Model extends Bundle {
+    //put your code here
+}
+
+?>
